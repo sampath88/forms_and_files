998aa88dc21f78e28c79f633214241ebcd7d6dd0
sampath88 <sampathreddy8896@gmail.com>
2023-09-02 15:00:05 +0530

    uploading a file to cloudinary

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # forms and files
+
+
+1. Set up your `.env` file
+   - Duplicate `.env.example` to `.env`
+   - Replace <YOUR_CLOUDINARY_NAME>, <YOUR_CLOUDINARY_API_KEY>, <YOUR_CLOUDINARY_SECRET> with your details.
--- index.js
+++ index.js
@@ -1,28 +1,50 @@
 require("dotenv").config();
 const express = require("express");
 const fileupload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
 
 const app = express();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 app.set("view engine", "ejs");
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); //for query parsing
-app.use(fileupload({
-  useTempFiles: true,
-  tempFileDir:"/tmp"
-}));
+app.use(
+  fileupload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
 app.get("/myget", (req, res) => {
   console.log(req.query);
   res.send(req.query);
 });
 
-app.post("/mypost", (req, res) => {
+app.post("/mypost", async (req, res) => {
   console.log(req.body);
   console.log(req.files);
-  res.send(req.body);
+
+  let file = req.files.samplefile;
+
+  const result = await cloudinary.uploader.upload(file.tempFilePath, {
+    folder: "users",
+  });
+
+  console.log(result);
+  const details = {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    result,
+  };
+  res.send(details);
 });
 
 app.get("/mygetform", (req, res) => {
